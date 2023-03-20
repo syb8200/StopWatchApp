ef52bf4590f9b72f9565a62b7a426d9e2531331a
syb8200 <syb8200@naver.com>
2023-03-21 00:38:29 +0900

    스탑워치 및 카운트다운 기능 구현 (Thread)

--- app/src/main/java/fastcampus/part1/fc_chapter6/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/fc_chapter6/MainActivity.kt
@@ -2,14 +2,20 @@ package fastcampus.part1.fc_chapter6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import fastcampus.part1.fc_chapter6.databinding.ActivityMainBinding
 import fastcampus.part1.fc_chapter6.databinding.DialogCountdownSettingBinding
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private var countdownSecond = 10
+    private var currentCountdownDeciSecond = countdownSecond * 10
+    private var currentDeciSecond = 0
+    private var timer : Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,10 +52,44 @@ class MainActivity : AppCompatActivity() {
             showCountdownSettingDialog()
         }
 
+        initCountdownViews()
+
+    }
+
+    private fun initCountdownViews() {
+        binding.countdownTextView.text = String.format("%02d", countdownSecond)
+
+        binding.countdownProgressBar.progress = 100
     }
 
     private fun start() {
+        // timer == Worker Thread 만드는 것
+        timer = timer(initialDelay = 0, period = 100) {
+            if (currentCountdownDeciSecond == 0) {
+                currentDeciSecond += 1
 
+                val minutes = currentDeciSecond.div(10) / 60
+                val seconds = currentDeciSecond.div(10) % 60
+                val deciSeconds = currentDeciSecond % 10
+
+                // [방법1] : runOnUiThread 사용하기 (이게 없으면 Worker Thread 에서 ui 작업을 진행하는 것이기 때문에 에러 발생)
+                runOnUiThread {
+                    binding.timeTextView.text = String.format("%02d:%02d", minutes, seconds)
+                    binding.tickTextView.text = deciSeconds.toString()
+                    binding.countdownGroup.isVisible = false
+                }
+            } else {
+                currentCountdownDeciSecond -= 1
+                val seconds = currentCountdownDeciSecond / 10
+                val progress = (currentCountdownDeciSecond / (countdownSecond * 10f)) * 100
+
+                // [방법2] : view에서 post하기
+                binding.root.post {
+                    binding.countdownTextView.text = String.format("%02d", seconds )
+                    binding.countdownProgressBar.progress = progress.toInt()
+                }
+            }
+        }
     }
 
     private fun stop() {
@@ -57,10 +97,18 @@ class MainActivity : AppCompatActivity() {
         binding.stopButton.isVisible = true
         binding.pauseButton.isVisible = false
         binding.lapButton.isVisible = false
+
+        currentDeciSecond = 0
+        binding.timeTextView.text = "00:00"
+        binding.tickTextView.text = "0"
+
+        binding.countdownGroup.isVisible = true
+        initCountdownViews()
     }
 
     private fun pause() {
-
+        timer?.cancel()
+        timer = null
     }
 
     private fun lap() {
@@ -82,7 +130,8 @@ class MainActivity : AppCompatActivity() {
             setTitle("카운트다운 설정")
             setPositiveButton("확인") { _, _ ->
                 countdownSecond = dialogBinding.countdownSecondPicker.value
-                // String format 사용 -> 01초, 02초 ... 형식 맞춰주기 위해서
+                currentCountdownDeciSecond = countdownSecond * 10
+                // String.format() 사용 -> 01초, 02초 ... 형식 맞춰주기 위해서
                 // %02d% : 00 형식의 숫자가 나타나게 된다.
                 binding.countdownTextView.text = String.format("%02d", countdownSecond)
             }
